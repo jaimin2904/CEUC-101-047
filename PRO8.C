d33a0402376d219bd8084be67e7c37ed34a668ec
jaimin2904 <jaiminkhambhaliya2904@gmail.com>
2025-07-28 14:58:43 +0530

    Add files via upload

---
+++ PRO8.C
@@ -0,0 +1,13 @@
+/*aim:
+ programeer:jaimin
+ date:28/07/25*/
+ #include<stdio.h>
+ #include<conio.h>
+ int main()
+ {  int age;
+   clrscr();
+   printf("enter your age:");
+   scanf("%d",&age);
+   age>=18?printf("you are eligible for opening an account\n"):printf("you are not eligible for opening an account");
+  getch();
+   }
