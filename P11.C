f4b847689934dc705298dae675ea17fee9552b2e
jaimin2904 <jaiminkhambhaliya2904@gmail.com>
2025-08-04 15:27:41 +0530

    Update P11.C

--- P11.C
+++ P11.C
@@ -11,7 +11,8 @@ int main()
   printf("enter your mark");
   scanf("%d",&m);
 
- (m>=0&&m<=100)? (m>=90&&m<=100)?printf("grade a"):(m>=80&&m<90)?printf("grade b"):(m>=70&&m<80)?printf("grade c"):(m>=60&&m<70)?printf("grade d"):printf("grede f"):printf("fail");
+ (m>=0&&m<=100)? (m>=90)?printf("grade a"):(m>=80)?printf("grade b"):(m>=70)?printf("grade c"):(m>=60)?printf("grade d"):printf("grede f"):printf("fail");
   return 0;
+
 
   }
