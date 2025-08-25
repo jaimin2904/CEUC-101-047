d5c68aa9ead984a483fb68a863d1b92f5a7b3040
jaimin2904 <jaiminkhambhaliya2904@gmail.com>
2025-08-25 18:37:25 +0530

    Add files via upload

---
+++ 123.C
@@ -0,0 +1,20 @@
+/* aim:
+  programmer:
+  data:25/8/25  */
+
+#include<stdio.h>
+#include<conio.h>
+  void main()
+{
+	  int r,c;
+	  clrscr();
+	  for(r=1;r<=10;r++)
+  {
+	  for(c=1;c<=5;c++)
+   {
+	 printf("%d\t",r*c);
+   }
+	 printf("\n");
+  }
+	 getch();
+}
