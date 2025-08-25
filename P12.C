8500295873394bc221f682447ceb7352bfb623f0
jaimin2904 <jaiminkhambhaliya2904@gmail.com>
2025-08-25 14:18:41 +0530

    Add files via upload

---
+++ P12.C
@@ -0,0 +1,22 @@
+/*aim
+  programeer:
+  date:
+  */
+ #include<stdio.h>
+ #include<conio.h>
+ void main()
+{
+  int n;
+  clrscr();
+  for(n=1;n<=50;n++)
+  {
+    printf("\nbook id:%d",n);
+
+    if(n%5==0)
+  {
+    printf("(special adition)",n);
+  }
+
+  }
+  getch();
+    }
