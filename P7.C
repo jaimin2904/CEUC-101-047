5f10281e1581e53dc03c544939942e579e075aa2
jaimin2904 <jaiminkhambhaliya2904@gmail.com>
2025-09-01 15:44:42 +0530

    Add files via upload

---
+++ P7.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+  clrscr();
+  printf("enter the your age:");
+  scanf("%d",&a);
+  a<=12?printf("you are eligible"):printf("you are not eligible");
+
+  getch();
+  }
