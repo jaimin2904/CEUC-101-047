9fea381e5f2dde9b6373428c811223b945702014
jaimin2904 <jaiminkhambhaliya2904@gmail.com>
2025-09-26 15:59:18 +0530

    Add files via upload

---
+++ P24-U-P.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   char n[200];
+   int i;
+   clrscr();
+   gets(n);
+     for(i=0;n[i]!='\0';i++)
+     {
+       if(n[i]>='A' && n[i]<='Z')
+       {
+	 n[i]=n[i]+32;
+       }
+     }
+     printf("%s",n);
+     return 0;
+     }
